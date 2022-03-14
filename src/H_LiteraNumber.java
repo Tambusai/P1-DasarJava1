0b9cd81966c3243d7c1450190e8889681c4d89cf
KHOIRUL PC <khoirulashabi17@gmail.com>
2022-03-14 17:28:16 +0700

    mencetak literal number

---
+++ src/H_LiteraNumber.java
@@ -0,0 +1,15 @@
+public class H_LiteraNumber
+{
+    public static void main(String[] args)
+    {
+        int distance=25;
+        final long MM_TOL_KM=1000000L;
+        float latilude=109.234567f;
+        final double PHI=3.14;
+        System.out.println("nilai literal int=" + distance);
+        System.out.println("nilai literal long=" +MM_TOL_KM);
+        System.out.println("nilai literal float=" + latilude);
+        System.out.println("nilai literal double=" + PHI);
+
+    }
+}
