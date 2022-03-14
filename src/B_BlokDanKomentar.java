f55f9b20938cce916c6163325cbe6ad875ed7362
KHOIRUL PC <khoirulashabi17@gmail.com>
2022-03-14 15:27:50 +0700

    percobaan whitespace di java

--- src/B_BlokDanKomentar.java
+++
@@ -1,1 +0,0 @@
-public class B_BlokDanKomentar { public static void main(String[] args) { System.out.println("pemogrmana berorientasi objek dengan java"); }}
---
+++ src/B_whitespace.java
@@ -0,0 +1,1 @@
+public class B_whitespace { public static void main(String[] args) { System.out.println("pemogrmana berorientasi objek dengan java"); }}
