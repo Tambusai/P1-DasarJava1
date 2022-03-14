5109ec58eeb869310ecd2f3a01bf18a6063f5ba8
KHOIRUL PC <khoirulashabi17@gmail.com>
2022-03-14 15:06:14 +0700

    initial project pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("pemograman Berorientasi Objek dengan Java");
+        //contoh komentar 1 baris
+
+        /*
+        contoh komentar 1 baris
+         */
+
+        /**
+         * Java Doc--> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
